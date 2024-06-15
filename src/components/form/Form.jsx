6c907fa6914c5473a3a4b61d1d6aff9e4c3b327e
chryschi <121822197+chryschi@users.noreply.github.com>
2024-06-15 13:28:09 +0200

    Add more styling to sidebar elements

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -11,7 +11,6 @@ const Form = ({
 }) => {
   const [dropped, setDropped] = useState(false);
   const [createMode, setCreateMode] = useState(false);
-  // const [visibilityList, setVisibilityList]
 
   const infoHandle = (e) => {
     infoHandler(e);
@@ -20,31 +19,61 @@ const Form = ({
 
   return (
     <section>
-      <div>
-        <button className="section" onClick={() => setDropped(!dropped)}>
-          <h2>{content.title}</h2>
+      <h2 className="drop-header">
+        <button className="drop-button" onClick={() => setDropped(!dropped)}>
+          <span className="material-symbols-outlined">
+            {dropped ? "keyboard_arrow_down" : "keyboard_arrow_right"}
+          </span>
+          {content.title}
         </button>
-      </div>
+        <button className="visibility">
+          <span className="material-symbols-outlined">visibility</span>
+        </button>
+      </h2>
 
       {dropped ? (
         <div className="drop-menu">
           {singleInfo
             ? null
-            : info.length !== 0 &&
-              info.map((station) => (
-                <p key={station.index}>
-                  {station[content.displayEntry]}
-                  <button
-                    onClick={() => visibilityHandler(station.id, station.index)}
-                  >
-                    show/hide
-                  </button>
-                </p>
-              ))}
+            : info.length !== 0 && (
+                <div className="bulletpoint-container">
+                  {info.map((station) => (
+                    <p key={station.index}>
+                      {station[content.displayEntry]}
+                      <div className="buttons">
+                        <button className="delete">
+                          <span className="material-symbols-outlined">
+                            delete_forever
+                          </span>
+                        </button>
+                        <button className="edit">
+                          <span className="material-symbols-outlined">
+                            edit
+                          </span>
+                        </button>
+                        <button
+                          className="visibility"
+                          onClick={() =>
+                            visibilityHandler(station.id, station.index)
+                          }
+                        >
+                          <span className="material-symbols-outlined">
+                            {station.visible ? "visibility" : "visibility_off"}
+                          </span>
+                        </button>
+                      </div>
+                    </p>
+                  ))}
+                </div>
+              )}
           <form onSubmit={(e) => infoHandle(e)}>
             {singleInfo ? null : !createMode ? (
-              <button onClick={() => setCreateMode(!createMode)}>
+              <button
+                className="add-button"
+                onClick={() => setCreateMode(!createMode)}
+              >
                 {content.newPointText}
+                <span className="material-symbols-outlined">add</span>
               </button>
             ) : null}
 
